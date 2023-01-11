f43db914a99e4181e0abfceb9f8117f668fc26e1
Leniza Souza <lenizarb@gmail.com>
2023-01-11 10:05:41 -0300

    cadastro de cliente

--- src/Usuários.java
+++ src/Usuários.java
@@ -3,47 +3,36 @@ import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
+//import utils.Utils;
+
 public class Usuários {
 
 	public static void main(String[] args) {
 		
 		Scanner l = new Scanner(System.in);
 		JOptionPane.showMessageDialog(null, "BEM VINDO AO CADASTRO DE USUÁRIOS");
-       // System.out.println("BEM VINDO AO CADASTRO DE USUÁRIOS");
-        
-        //System.out.println("Digite os dados: "+"\nNome ");
-		//JOptionPane.showMessageDialog(null, "Digite os dados: "+"\nNome ");
-		JOptionPane.showInputDialog("Digite os dados: "+"\nNome ");
-		
-       // String nome = l.nextLine();
-        
-        JOptionPane.showInputDialog("Login desejado ");
-        
-        //String login = l.nextLine();
-        
-        JOptionPane.showInputDialog("Cargo ");
-        
-        //String cargo = l.nextLine();
-        
-        JOptionPane.showInputDialog("Senha de acesso");
-        
-        //int senha = l.nextInt();
-        
-        
+              
+       	String nome = JOptionPane.showInputDialog("Digite os dados: "+"\nNome ");
+		        
+		String login = JOptionPane.showInputDialog("Login desejado ");
+                    
+		String cargo  = JOptionPane.showInputDialog("Cargo ");
+                
+		String senha = JOptionPane.showInputDialog("Senha de acesso");
+                
         Dados usuario;
         
         usuario = new Dados();
         
-       // usuario.setNome(nome);
-        //usuario.setLogin(login);
-        //usuario.setCargo(cargo);
-        
-        
+        usuario.setNome(nome);
+        usuario.setLogin(login);
+        usuario.setCargo(cargo);            
+      
         ArrayList<Dados> usuarios = new ArrayList<Dados>();
         
         usuarios.add(usuario);
-        
-        //System.out.println(usuario.getNome()+"\n"+usuario.getLogin()+"\n"+usuario.getCargo());
+                
         JOptionPane.showMessageDialog(null,usuario.getNome()+"\n"+usuario.getLogin()+"\n"+usuario.getCargo());
+  
      }
 }
